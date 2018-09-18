d82b60f8b72bf00b1e69a815b4624895264a3432
Creed <elmolangcap@gmail.com>
2018-09-18 12:06:30 +0800

    home and footer style fix

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { View } from "react-native";
+import { View, TouchableOpacity, Image } from "react-native";
 
 class Footer extends React.Component {
   render() {
     return(
-      <View style={{ height: 100, width: "100%", backgroundColor: "red" }}>
-
+      <View style={{ height: 75, width: "100%", backgroundColor: "#f2f2f2", flexDirection: "row", justifyContent: "center", alignItems: "center", borderTopWidth: 1, borderStyle: "solid", borderColor: "#d9d9d9" }}>
+        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require('../../assets/icons/home-icon.png')}
+            />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center"  }}>
+            <Image
+              style={{ height: 48, width: 32 }}
+              source={require('../../assets/icons/watericon.png')}
+            />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center"  }}>
+            <Image
+              style={{ height: 50, width: 36 }}
+              source={require('../../assets/icons/planticon.png')}
+            />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center"  }}>
+            <Image
+              style={{ height: 45, width: 45 }}
+              source={require('../../assets/icons/settingicn.png')}
+            />
+        </TouchableOpacity>
       </View>
     )
   }
--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 class Home extends React.Component {
   render() {
     return(
-      <View>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
-          <Text>elmo    </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Image source={require('../assets/logo/logo3.png')} style={{ height: 253 , width: 170 }}/>
       </View>
     )
   }
